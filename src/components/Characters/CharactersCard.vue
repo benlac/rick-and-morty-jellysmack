9c7bba03ee96a48fb9aea821c3dbe402ccbd01ad
benlac <benoit.lacombled@gmail.com>
2022-04-25 11:38:02 +0200

    feat(CharactersCard): better hover animation on the cards

--- src/components/Characters/CharactersCard.vue
+++ src/components/Characters/CharactersCard.vue
@@ -2,7 +2,7 @@
   <router-link :to="{ name: 'details', params: { id } }">
     <div class="card">
       <img class="card__img" :src="image" :alt="`picture of ${name}`" />
-
+      <p class="card__read__more">Read more about this character</p>
       <div class="card__wrapper__text">
         <p class="card__text">{{ name }}</p>
       </div>
@@ -26,7 +26,6 @@ a {
 .card {
   width: 300px;
   height: 270px;
-  background: #ffffff;
   border-radius: 27px;
   margin: 1rem;
   display: flex;
@@ -37,17 +36,27 @@ a {
   box-shadow: 0 0 5px 0 hsla(0, 0%, 47.1%, 0.3);
   transition-duration: 0.3s;
   position: relative;
+  background: #000;
+
+  .card__read__more {
+    display: none;
+    position: absolute;
+    top: 37%;
+    border: 50%;
+    bottom: 50%;
+    color: #fff;
+    text-decoration: underline;
+    font-size: 0.8em;
+  }
   &:hover {
     transform: scale(1.02);
     box-shadow: 0 10px 20px 0 hsla(0, 0%, 47.1%, 0.3);
-
     .card__img {
-      transform: translatey(-10px) translatex(-5px) scale(1.05);
-      transition: 0.3s;
-      margin-top: -20px;
+      opacity: 0.5;
     }
-    .card__wrapper__text {
-      height: 45%;
+
+    .card__read__more {
+      display: block;
     }
   }
   .card__img {
@@ -64,6 +73,7 @@ a {
     display: flex;
     justify-content: center;
     align-items: center;
+    background: #fff;
 
     .card__text {
       color: #44281d;
